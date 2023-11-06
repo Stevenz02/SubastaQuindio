9ca35c2c6fe7c1166ab384aa6f3fea2b06f59284
Stevenz02 <129470364+Stevenz02@users.noreply.github.com>
2023-11-05 22:12:17 -0500

    Actualizacion Module Info
    Se actualizo elmodule info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,8 +4,5 @@ module co.edu.uniquindio.subastaq.subastaq {
     requires org.mapstruct;
     requires java.desktop;
     requires java.logging;
-
-    opens co.edu.uniquindio.subastaq.subastaq.aplicacion to javafx.fxml;
-    opens co.edu.uniquindio.subastaq.subastaq.controller to javafx.fxml;
-    exports co.edu.uniquindio.subastaq.subastaq.aplicacion to javafx.graphics;
+    
 }
