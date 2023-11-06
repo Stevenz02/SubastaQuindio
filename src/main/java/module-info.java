5e0c89b676abac11b83e1804e7fb37bd0b318e2e
Stevenz02 <129470364+Stevenz02@users.noreply.github.com>
2023-11-05 22:09:57 -0500

    Creacion vistas
    Se crearon las vistas de la ventana principal del inicio de sesion, adicional se crearon la vista de autenticar y registrar usuario

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module co.edu.uniquindio.subastaq.subastaq {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.mapstruct;
+    requires java.desktop;
+    requires java.logging;
 
-
-    opens co.edu.uniquindio.subastaq.subastaq to javafx.fxml;
-    exports co.edu.uniquindio.subastaq.subastaq;
+    opens co.edu.uniquindio.subastaq.subastaq.aplicacion to javafx.fxml;
+    opens co.edu.uniquindio.subastaq.subastaq.controller to javafx.fxml;
+    exports co.edu.uniquindio.subastaq.subastaq.aplicacion to javafx.graphics;
 }
