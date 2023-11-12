d61e7c99593b3e45ecc912efaaca97b3ac510a7f
Stevenz02 <129470364+Stevenz02@users.noreply.github.com>
2023-11-12 17:55:18 -0500

    Vistas y configuracion
    Se realizo vistas de comprador y adicional metodos en gestionarAnunciosController

--- src/main/java/co/edu/uniquindio/subastaq/subastaq/controller/pujarProductoController.java
+++ src/main/java/co/edu/uniquindio/subastaq/subastaq/controller/pujarProductoController.java
@@ -1,4 +1,55 @@
 package co.edu.uniquindio.subastaq.subastaq.controller;
 
+import co.edu.uniquindio.subastaq.subastaq.mapping.dto.AnuncioDto;
+import co.edu.uniquindio.subastaq.subastaq.mapping.dto.ProductoDto;
+import co.edu.uniquindio.subastaq.subastaq.model.TipoProducto;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 public class pujarProductoController {
+
+    @FXML
+    private Button buttonOfertar;
+
+    @FXML
+    private TableColumn<ProductoDto, String> colmNombreProducto;
+
+    @FXML
+    private TableColumn<AnuncioDto, Double> colmPrecio;
+
+    @FXML
+    private TableColumn<ProductoDto, TipoProducto> colmTipoProducto;
+
+    @FXML
+    private ImageView imgVistaProducto;
+
+    @FXML
+    private Label labelFechaFinal;
+
+    @FXML
+    private Label labelFechaInicial;
+
+    @FXML
+    private Label labelNombreProducto;
+
+    @FXML
+    private Label labelTipoProducto;
+
+    @FXML
+    private TableView<?> tableAnuncios;
+
+    @FXML
+    private TextField textFieldPuja;
+
+    @FXML
+    void handleOfertar(ActionEvent event) {
+
+    }
+
 }
