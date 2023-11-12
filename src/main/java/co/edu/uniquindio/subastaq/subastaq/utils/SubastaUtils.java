51ffc3ef6040009e88c1de6e3687fa92030117d7
Stevenz02 <129470364+Stevenz02@users.noreply.github.com>
2023-11-11 19:56:52 -0500

    Update errores
    Update eliminar usuario

--- src/main/java/co/edu/uniquindio/subastaq/subastaq/utils/SubastaUtils.java
+++ src/main/java/co/edu/uniquindio/subastaq/subastaq/utils/SubastaUtils.java
@@ -9,45 +9,7 @@ public class SubastaUtils {
 
     public static SubastaUniquindio inicializarDatos() {
         SubastaUniquindio subastaUniquindio = new SubastaUniquindio();
-
-        Usuario usuario = new Usuario();
-        usuario.setNombre("juan 2");
-        usuario.setApellido("arias");
-        usuario.setCedula("125454");
-        usuario.setEdad(18);
-        usuario.setTipo("Comprador");
-        usuario.setNombreUsuario("admins");
-        usuario.setContrasenia("contra");
-        subastaUniquindio.getListaUsuarios().add(usuario);
-
-        Usuario usuario2 = new Usuario();
-        usuario2.setNombre("juan 3");
-        usuario2.setApellido("ari2s");
-        usuario2.setCedula("125454");
-        usuario2.setEdad(32);
-        usuario2.setTipo("Anunciante");
-        usuario2.setNombreUsuario("admins");
-        usuario2.setContrasenia("contra");
-        subastaUniquindio.getListaUsuarios().add(usuario2);
-
-        Comprador comprador = new Comprador();
-        comprador.setNombre("juan 3");
-        comprador.setApellido("ari2s");
-        comprador.setCedula("125454");
-        comprador.setEdad(32);
-        comprador.setNombreUsuario("admins");
-        comprador.setContrasenia("contra");
-        subastaUniquindio.getListaCompradores().add(comprador);
-
-
-        Anunciante anunciante = new Anunciante();
-        anunciante.setNombre("juan 4");
-        anunciante.setApellido("ari2s");
-        anunciante.setCedula("125454");
-        anunciante.setEdad(32);
-        anunciante.setNombreUsuario("admins");
-        anunciante.setContrasenia("contra");
-        subastaUniquindio.getListaAnunciantes().add(anunciante);
+        subastaUniquindio = Persistencia.cargarRecursoSubastaXML();
 
         System.out.println("Información de la subasta creada");
         return subastaUniquindio;
