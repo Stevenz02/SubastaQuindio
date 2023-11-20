f91f870763c37b1eb81b1c93fd18dfa31d235783
Stevenz02 <129470364+Stevenz02@users.noreply.github.com>
2023-11-20 16:25:48 -0500

    Se muestra toda la informacion en las tablas de anuncios
    Ya se muestra la información de todos los anuncios en todas las vistas

--- src/main/java/co/edu/uniquindio/subastaq/subastaq/controller/gestionarPujasController.java
+++ src/main/java/co/edu/uniquindio/subastaq/subastaq/controller/gestionarPujasController.java
@@ -1,15 +1,92 @@
 package co.edu.uniquindio.subastaq.subastaq.controller;
 
+import co.edu.uniquindio.subastaq.subastaq.controllerModel.AnuncioController;
+import co.edu.uniquindio.subastaq.subastaq.controllerModel.ModelFactoryController;
+import co.edu.uniquindio.subastaq.subastaq.mapping.dto.AnuncioDto;
+import co.edu.uniquindio.subastaq.subastaq.mapping.dto.ProductoDto;
+import co.edu.uniquindio.subastaq.subastaq.model.TipoProducto;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class gestionarPujasController {
-    public TableView tableAnuncios;
-    public TableView tablePujas;
-    public TableColumn columnComprador;
-    public TableColumn columnCodigoPuja;
-    public TableColumn columnFecha;
-    public TableColumn columnOferta;
-    public Button buttonElegirPuja;
+
+    ModelFactoryController modelFactoryController;
+    AnuncioController AnuncioControllerService;
+    ObservableList<AnuncioDto> listaAnunciosDto = FXCollections.observableArrayList();
+    AnuncioDto anuncioSeleccionado;
+    public ProductoDto productoDto;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button buttonElegirPuja;
+
+    @FXML
+    private TableColumn<?, ?> columnCodigoPuja;
+
+    @FXML
+    private TableColumn<?, ?> columnComprador;
+
+    @FXML
+    private TableColumn<?, ?> columnFecha;
+
+    @FXML
+    private TableColumn<?, ?> columnOferta;
+
+    @FXML
+    private TableView<?> tablePujas;
+
+    @FXML
+    private TableColumn<AnuncioDto, String> colmNombreProducto;
+
+    @FXML
+    private TableColumn<AnuncioDto, String> colmFechaPublicacion;
+
+    @FXML
+    private TableColumn<AnuncioDto, String> colmFechaFinalizacion;
+
+    @FXML
+    private TableView<AnuncioDto> tableAnuncios;
+
+    @FXML
+    void initialize() {
+        AnuncioControllerService = new AnuncioController();
+        modelFactoryController = ModelFactoryController.getInstance();
+        intiView();
+    }
+    private void intiView() {
+        initDataBinding();
+        obtenerAnuncios();
+        tableAnuncios.getItems().clear();
+        tableAnuncios.setItems(listaAnunciosDto);
+        listenerSelection();
+    }
+    private void initDataBinding() {
+        // Configura las columnas con los nombres de las propiedades
+        colmNombreProducto.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().productoDto().nombreProducto()));
+        colmFechaPublicacion.setCellValueFactory(cellData -> new SimpleStringProperty(String.valueOf(cellData.getValue().fechaPublicacion())));
+        colmFechaFinalizacion.setCellValueFactory(cellData -> new SimpleStringProperty(String.valueOf(cellData.getValue().FechaLimite())));
+    }
+    private void obtenerAnuncios() {
+        listaAnunciosDto.addAll(AnuncioControllerService.obtenerAnuncios());
+    }
+    private void listenerSelection() {
+        tableAnuncios.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+            anuncioSeleccionado = newSelection;
+        });
+    }
+
 }
+
--- src/main/java/co/edu/uniquindio/subastaq/subastaq/controller/pujarProductoController.java
+++ src/main/java/co/edu/uniquindio/subastaq/subastaq/controller/pujarProductoController.java
@@ -1,9 +1,14 @@
 package co.edu.uniquindio.subastaq.subastaq.controller;
 
+import co.edu.uniquindio.subastaq.subastaq.controllerModel.AnuncioController;
 import co.edu.uniquindio.subastaq.subastaq.controllerModel.ModelFactoryController;
 import co.edu.uniquindio.subastaq.subastaq.mapping.dto.AnuncioDto;
 import co.edu.uniquindio.subastaq.subastaq.mapping.dto.ProductoDto;
+import co.edu.uniquindio.subastaq.subastaq.model.Anuncio;
 import co.edu.uniquindio.subastaq.subastaq.model.TipoProducto;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -13,23 +18,28 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class pujarProductoController {
 
-    public TableColumn colmNombreAnunciante;
-    public TableColumn colmCodigo;
-    private ModelFactoryController modelFactoryController;
+    ModelFactoryController modelFactoryController;
+    AnuncioController AnuncioControllerService;
+    ObservableList<AnuncioDto> listaAnunciosDto = FXCollections.observableArrayList();
+    AnuncioDto anuncioSeleccionado;
+    public ProductoDto productoDto;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnCerrarSesion;
 
     @FXML
     private Button buttonOfertar;
-
-    @FXML
-    private TableColumn<ProductoDto, String> colmNombreProducto;
-
-    @FXML
-    private TableColumn<AnuncioDto, Double> colmPrecio;
-
-    @FXML
-    private TableColumn<ProductoDto, TipoProducto> colmTipoProducto;
 
     @FXML
     private ImageView imgVistaProducto;
@@ -47,16 +57,23 @@ public class pujarProductoController {
     private Label labelTipoProducto;
 
     @FXML
-    private TableView<?> tableAnuncios;
-
-    @FXML
     private TextField textFieldPuja;
 
     @FXML
+    private TableColumn<AnuncioDto, String> colmNombreAnunciante;
+
+    @FXML
+    private TableColumn<AnuncioDto, String> colmNombreProducto;
+
+    @FXML
+    private TableColumn<AnuncioDto, String> colmFechaLimite;
+
+    @FXML
+    private TableView<AnuncioDto> tableAnuncios;
+
+    @FXML
     void handleOfertar(ActionEvent event) {
-
     }
-
     public void cerrarSesionAction(ActionEvent event) {
         modelFactoryController = ModelFactoryController.getInstance();
         if (modelFactoryController != null) {
@@ -65,5 +82,32 @@ public class pujarProductoController {
             // Manejar el caso en que modelFactoryController es null
             System.out.println("modelFactoryController es null");
         }
+    }
+    @FXML
+    void initialize() {
+        AnuncioControllerService = new AnuncioController();
+        modelFactoryController = ModelFactoryController.getInstance();
+        intiView();
+    }
+    private void intiView() {
+        initDataBinding();
+        obtenerAnuncios();
+        tableAnuncios.getItems().clear();
+        tableAnuncios.setItems(listaAnunciosDto);
+        listenerSelection();
+    }
+    private void initDataBinding() {
+        // Configura las columnas con los nombres de las propiedades
+        colmNombreAnunciante.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().anuncianteDto().nombre()));
+        colmNombreProducto.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().productoDto().nombreProducto()));
+        colmFechaLimite.setCellValueFactory(cellData -> new SimpleStringProperty(String.valueOf(cellData.getValue().FechaLimite())));
+    }
+    private void obtenerAnuncios() {
+        listaAnunciosDto.addAll(AnuncioControllerService.obtenerAnuncios());
+    }
+    private void listenerSelection() {
+        tableAnuncios.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+            anuncioSeleccionado = newSelection;
+        });
     }
 }
