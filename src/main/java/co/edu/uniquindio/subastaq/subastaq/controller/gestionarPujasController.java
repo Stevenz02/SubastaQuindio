c2fa995b08f845739ceb7be3296adce066d996a5
Stevenz02 <129470364+Stevenz02@users.noreply.github.com>
2023-11-19 23:46:05 -0500

    Se ajustaron cambios en las vistas
    Se hicieron pequeños cambios de diseño en las vistas

--- src/main/java/co/edu/uniquindio/subastaq/subastaq/controller/gestionarPujasController.java
+++ src/main/java/co/edu/uniquindio/subastaq/subastaq/controller/gestionarPujasController.java
@@ -1,15 +1,15 @@
 package co.edu.uniquindio.subastaq.subastaq.controller;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 public class gestionarPujasController {
     public TableView tableAnuncios;
-    public TableColumn columnNombre;
-    public TableColumn columnCodigo;
     public TableView tablePujas;
     public TableColumn columnComprador;
     public TableColumn columnCodigoPuja;
     public TableColumn columnFecha;
     public TableColumn columnOferta;
+    public Button buttonElegirPuja;
 }
